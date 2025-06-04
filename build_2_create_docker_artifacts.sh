9845b80b6d5f83d463476597ecc6e9ba9defb83f
Brandon Miller <millertimebjm@hotmail.com>
2025-06-04 00:58:21 -0500

    add build ci/cd artifacts

---
+++ build_2_create_docker_artifacts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Delete docker container FORCED if it exists
+# Your image name
+IMAGE_NAME="profile"
+
+dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
+docker run -d -p 10102:8080 --restart=always --name $IMAGE_NAME -e "Values:Profile:AppConfigConnection=$1" $IMAGE_NAME:latest
+rm -rf /tmp/Containers
+
+# Check if there are any dangling images
+if [ -n "$(docker images -f dangling=true -q)" ]; then
+    # Remove dangling images
+    docker rmi --force $(docker images -f "dangling=true" -q)
+else
+    echo "No dangling images found."
+fi
